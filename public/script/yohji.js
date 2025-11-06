0911f6a0028fa1f0d92195abce177de60cdf3634
hidekkia <vitor.anzai@sptech.school>
2025-11-05 21:54:52 -0300

    Versão inicial da página do Yohji

---
+++ public/script/yohji.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menu-btn');
+const sideMenu = document.getElementById('side-menu');
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.classList.toggle('active');
+});
